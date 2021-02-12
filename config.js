adb22dc0c472d6b9d3275fc20a997ae18b5dd1c0
geom4001t3 <78328757+geom4001t3@users.noreply.github.com>
2021-02-11 20:45:23 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     showMarkers: true,
     markerColor: '#3FB1CE',
     theme: 'dark',
-    use3dTerrain: false,
+    use3dTerrain: true,
     title: 'What to do in Gatineau Park?',
     subtitle: 'Gatineau Park is the National Capital Region’s conservation park. The largest green space in the region, the Park occupies an area of more than 361 square kilometres, and is a place of rich and unique biodiversity. It is the second-most visited park in Canada, and a destination for outdoor enthusiasts to engage in recreational activities that respect the environment. (NCC, 2020)',
     byline: 'By Alexandra, Didier, Edward, Gillian, Yussuf',
@@ -12,7 +12,9 @@ var config = {
     chapters: [
         {
             id: 'overview-id',
-            hidden: true,
+            alignment: 'left',
+            description: '<h2>Gatineau park isn’t your everyday park. There are many activities to do and they slightly differ by season.</h2><br><h3>Summer Activities:</h3><ul><li> Hiking and walking </li><li> Dog walking </li><li> Cycling </li><li> In-line skating </li><li> Picnicking </li><li> Swimming </li><li> Boating Fishing </li><li> Horseback Riding</li><li> Rock climbing </li><li> Camping </li></ul><br><h3>Winter Activities:</h3><ul><li> Cross country skiing </li><li> Snowshoeing </li><li> Snow biking </li><li> In-line skating </li><li> Winter hiking and walking </li><li> Camping </li></ul><video width="500" height="300" controls><source src="./images/Gatineau_video.mp4" type="video/mp4">Your browser does not support the video tag.</video>Video taken on NCC`s Youtube page: <a href="https://www.youtube.com/watch?v=KZMiPpAMlKI&t=1s" target="_blank"Welcome to Gatineau Park</a>',
+            hidden: false,
             location: {
                 center: [-75.95286, 45.55058],
                 zoom: 10.74,
@@ -33,14 +35,14 @@ var config = {
                }
             ]
         },
+
         {
             id: 'mackenzie-king-id',
             alignment: 'left',
             hidden: false,
             title: 'Mackenzie King Estate',
             image: './images/Mack.png',
-            description: 'Canada’s 10th serving Prime Minister William Lyon Mackenize King gifted his 231 hectare country estate for all Canadians to enjoy in the 1950s. This estate in Gatineau Park offers a chance to engage with historical french gardens, hiking trails, and interactive exhibits. <br>For more information about Mackenzie King Estate visit: <a href="https://ncc-ccn.gc.ca/places/mackenzie-king-estate" target="_blank">Mackenzie King Estate (NCC)</a>'
-            ,
+            description: 'Canada’s 10th serving Prime Minister William Lyon Mackenize King gifted his 231 hectare country estate for all Canadians to enjoy in the 1950s. This estate in Gatineau Park offers a chance to engage with historical french gardens, hiking trails, and interactive exhibits. <br>For more information about Mackenzie King Estate visit: <a href="https://ncc-ccn.gc.ca/places/mackenzie-king-estate" target="_blank">Mackenzie King Estate (NCC)</a>',
             location: {
                 center: [-75.84786, 45.48358],
                 zoom: 15.5,
@@ -149,7 +151,7 @@ var config = {
 
         {
             id: 'Luskville-id',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
             title: 'Luskville Waterfalls',
             image: './images/waterfall.jpg',
@@ -204,7 +206,7 @@ var config = {
             hidden: false,
             title: 'Village of Wakefield',
             image: './images/wakefield.png',
-            description: 'Wakefield is a small, joyful village in the Municipality of La Pêche which is located on the western shore of the Gatineau River. Filled with local restaurant and small boutiques, Wakefield has plenty of attractions and activities to stay occupied for a day. <br><br>Wakefield covered bridge: Originally built in 1915, the Wakefield Covered Bridge links the two shores of the Gatineau River. Unfortunately in 1984 it was destroyed by a fire, however, it was later rebuilt by the community. Nowadays, it can only be used by pedestrians and some of its original features were maintained such as the pillars that support it. <br><br>Wakefield mill: Located on the banks of the La Pêche River the Wakefield Mill was initially built in 1838, however it was destroyed by a fire in1910. Today, the newly renovated mill is a central point along with the award-winning Wakefield Mill Hotel & Spa right beside. <br><br>Other activities & nearby points of interest include: Canoe, kayak & paddleboard rentals, mountain biking, golfing, alpine skiing (Vorlage, Edelweiss, Mont Cascades), cross-country skiing and more. <br>For more information about Wakefield visit: <a href="http://www.destinationwakefield.com/" target="_blank">Wakefield</a>',
+            description: 'Wakefield is a small, joyful village in the Municipality of La Pêche which is located on the western shore of the Gatineau River. Filled with local restaurant and small boutiques, Wakefield has plenty of attractions and activities to stay occupied for a day. <br><h3><u>Some historical sites:</u></h3> <h4>Wakefield covered bridge:</h4> <ul><li>Originally built in 1915, the Wakefield Covered Bridge links the two shores of the Gatineau River. Unfortunately in 1984 it was destroyed by a fire, however, it was later rebuilt by the community. Nowadays, it can only be used by pedestrians and some of its original features were maintained such as the pillars that support it.</li></ul><h4>Wakefield mill:</h4> <ul><li>Located on the banks of the La Pêche River the Wakefield Mill was initially built in 1838, however it was destroyed by a fire in1910. Today, the newly renovated mill is a central point along with the award-winning Wakefield Mill Hotel & Spa right beside.</li></ul><h3>Other activities & nearby points of interest include:</h5><ol><li>Canoe, kayak & paddleboard rentals</li> <li>Mountain biking</li> <li>Golfing</li> <li>Alpine skiing (i.e. Vorlage, Edelweiss, Mont Cascades)</li> <li>Cross-country skiing and more!</li> <br>For more information about Wakefield visit: <a href="http://www.destinationwakefield.com/" target="_blank">Wakefield</a>',
             location: {
                 center: [-75.92755, 45.64980],
                 zoom: 14,
@@ -228,7 +230,7 @@ var config = {
 
         {
             id: 'peche-lake-id',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
             title: 'La Pêche Lake',
             image: './images/lacpeche.jpg',
